b1355306c34224edf8b48df29c0bd06e9bc98e31
Shivansu77 <shivansubisht77@gmail.com>
2025-07-08 18:47:44 +0530

    Initial commit

---
+++ demoshopapp/src/components/Item.js
@@ -0,0 +1,10 @@
+import './Item.css';
+function ItemDate(){
+    return (
+        <header>
+            <h1>Item Date</h1>
+            <p>This is the item date component.</p>
+        </header>
+    )
+}
+export default ItemDate;
---
+++ demoshopapp/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import ItemDate from './components/Item';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <header>
+    <App />
+  <ItemDate />
+  </header>
+
+);
+
